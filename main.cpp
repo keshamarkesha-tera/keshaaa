a85d3bd05a169bb7cbe95f88c344d9be75f237c9
keshamarkesha-tera <135645279+keshamarkesha-tera@users.noreply.github.com>
2023-06-08 18:29:25 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
-int fibonacci(int n) {
+
+int fibonacci3(int n) {
     if (n <= 1)
         return n;
-
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    int fib3[n + 1];
+    fib3[0] = 0;
+    fib3[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        fib3[i] = fib3[i - 1] + fib3[i - 2];
+    }
+    return fib3[n];
 }
-
 int main() {
     int n;
-    std::cout << "Enter the number of Fibonacci numbers: ";
-    std::cin >> n;
-
-    std::cout << "The sequence of Fibonacci numbers: ";
-    for (int i = 0; i < n; i++) {
-        std::cout << fibonacci(i) << " ";
-    }
-
+    cout << "Enter the number n: ";
+    cin >> n;
+    cout << "Fibonacci Numbers: " << fibonacci3(n) << endl;
     return 0;
 }
